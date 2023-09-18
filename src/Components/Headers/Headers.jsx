aa8e5decd018bd338a26f7435c7c943a250d0d13
jerin-arpa <ismatjerrin3@gmail.com>
2023-09-19 04:57:32 +0600

    nested router concept

---
+++ src/Components/Headers/Headers.jsx
@@ -0,0 +1,16 @@
+
+
+const Headers = () => {
+    return (
+        <div>
+            <h2>Navbar</h2>
+            <nav>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact Us</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Headers;
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './components/Home/Home';
+import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello From React Router</div>,
-  },
-  {
-    path: '/about',
-    element: <div>I am in the about page</div>
-  },
-  {
-    path: '/contact',
-    element: <div>Call me</div>
+    element: <div><Home></Home> </div>,
+    children: [
+      {
+        path: '/about',
+        element: <About></About>
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>
+      }
+    ]
   }
 ]);
 
